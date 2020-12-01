9fcdbf03533f6cc7d5c73c7172c5f81c77a0c065
ELNachomacho <ELNachomacho@users.noreply.github.com>
2020-12-01 21:47:19 +0100

    update 1.4
    path way fixed

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BMI calculate Made By Jay</title>
+<style>
+    Header{
+        background:cornsilk;
+        float:left;
+            width:20%;
+            margin:0%;
+            padding:1%;
+    }
+    H1{
+        Color:rgb(0, 160, 200);
+    }
+    Label{
+        color:rgb(218, 0, 73);
+        font-weight: bold;
+    }
+ 
+</Style>
+</head>
+<body>
+<header>
+        <h1>BMI Calculator</h1>
+<article>
+<form method="POST" action="php/index.php">
+<label for="text">Weight in kilogram (kg):</label><br>
+<input type="text" name="weight">
+<br> 
+<label for="text">Height in meter (m):</label><br>
+<input type="text" name="height"> 
+<br>
+<input type="submit" name="submit" value="submit">
+</form>
+</article>
+</body>
+</html>
+</html>
