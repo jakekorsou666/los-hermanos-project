11478c6203dacd483657c73e0cb2b129d85ec35a
ELNachomacho <ELNachomacho@users.noreply.github.com>
2020-12-05 20:34:57 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -12,32 +12,62 @@
         }
     </Style>
 <body>
-    <h1>
-<?php 
-if ($_POST['submit']) { 
-$weight = $_POST['weight'];
-$height = $_POST['height'];
-function bmi($weight,$height) {
-$bmi = $weight/($height*$height);
-return $bmi;
-}
-
-$bmi = bmi($weight,$height);
-    if ($bmi <= 18.5) {
-    $output = "hey boney start eating";
-    }else if ($bmi >18.5 and $bmi<=24.9){
-    $output =  "aight you look good";
-    }else if ($bmi > 24.9 and $bmi <=29.9){
-    $output = "hmm u might be getting fat";
-    }else if($bmi > 30.0){
-        $output =  "Bitch you Fat As fuck!!";
-    }
-echo "Your BMI value is " . round($bmi,1) ;
-echo "<br>";
-echo  $output;
+<div>
+        <?php 
+        if ($_POST['submit']) { 
+        $weight = $_POST['weight'];
+        $height = $_POST['height'];
+        function bmi($weight,$height) {
+        $bmi = $weight/($height*$height);
+        return $bmi;
+        }
+        $bmi = bmi($weight,$height);
+            if ($bmi <= 18.5) {
+            $output = "hey boney start eating";
+            }else if ($bmi >18.5 and $bmi<=24.9){
+            $output =  "aight you look good";
+            }else if ($bmi > 24.9 and $bmi <=29.9){
+            $output = "hmm u might be getting fat";
+            }else if($bmi > 30.0){
+                $output =  "Bitch you Fat As fuck!!";
+            }
+        
+        echo "<br>";
+        echo  $output;
 }
 ?>
-    </h1>
+    </div>
+
+        <div>
+            <?PHP
+            echo "Your BMI value is " . round($bmi,1) ;
+            ?>
+        </div>
+
+        <div>
+        <?PHP
+        echo "<br>";
+        ?>
+        </div>
+
+        <div class='ww'>
+        <?PHP
+        switch ($output)
+        {
+            case "Bitch you Fat As fuck!!":
+                echo $output;
+                break ;
+        }
+        ?>
+        </div>
+
+        <div class='zaza'>
+        <?PHP
+       echo  $output;
+        ?>
+        </div>
+
+    <div></div>
 </body>
 </html>
 </html>
